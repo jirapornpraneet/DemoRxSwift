1a4c035029098bec3449d8da68062e021f9cbcba
“nut.j” <nut.j@finnomena.com>
2023-08-23 12:12:41 +0700

    Initial Commit

---
+++ DemoRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoRxSwift
+//
+//  Created by Nut Jiraporn on 23/8/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DemoRxSwiftUITests/DemoRxSwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DemoRxSwiftUITestsLaunchTests.swift
+//  DemoRxSwiftUITests
+//
+//  Created by Nut Jiraporn on 23/8/2566 BE.
+//
+
+import XCTest
+
+final class DemoRxSwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
